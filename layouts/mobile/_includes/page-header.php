c8ba4c654bf9a2eb95991ff7121f2716834246b0
Jae-young, Lim <limja38@hanmail.net>
2014-11-13 03:25:56 +0900

    Update page-header.php
    s1001은 관리자에게 필요한 코멘트인거 같습니다. 관리자 로그인 없이도 s1001이 해당 페이지에 보입니다. (layouts/default 데스트탑 레이아웃도 동일합니다.)

--- layouts/mobile/_includes/page-header.php
+++ layouts/mobile/_includes/page-header.php
@@ -1,6 +1,8 @@
 <div class="page-header rb-page-header">
 	<h1><?php echo $_FHM['name']?></h1>
 	<p>
-		<?php echo $_FHM['addinfo']?$_FHM['addinfo']:_LANG('s1001','xlayout')?>
+		<?php if($my['admin']):?>
+			<?php echo $_FHM['addinfo']?$_FHM['addinfo']:_LANG('s1001','xlayout')?>
+		<?php endif?>	
 	</p>
 </div>
